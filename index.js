a72ed989dbcc41ee8160369dc11eda4f32fa91fa
Omar Sarfraz <omar786089@gmail.com>
2022-09-11 01:16:58 +0500

    Changed Vercel Config

--- index.js
+++ index.js
@@ -26,3 +26,5 @@ const PORT = process.env.PORT || 5000
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`)))
     .catch((error) => console.error(error))
+
+module.exports = app;
